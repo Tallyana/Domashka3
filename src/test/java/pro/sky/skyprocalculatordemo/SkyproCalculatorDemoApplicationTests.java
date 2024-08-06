40bc44d1f455644767204532aab83693adb815bd
Tallyana <iggla67@yandex.ru>
2024-08-06 12:42:53 +0400

    4

---
+++ src/test/java/pro/sky/skyprocalculatordemo/SkyproCalculatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprocalculatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproCalculatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
